ab4af9208ba07f76cf6ba479a868228fd16a0447
tala123456 <tanja.landolt@zhdk.ch>
2022-10-20 11:11:55 +0200

    Exc_2
    publish exc 1 to github

---
+++ 01_20_oktober_/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(200, 250, 100);
+    noStroke();
+    fill(240, 150, 150);
+    rect(100, 100, 100, 100);
+    
+  }
