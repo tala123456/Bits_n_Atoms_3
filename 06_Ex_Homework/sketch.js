223b87d5a0a6d3f4d2df77700d927d790bed794c
tala123456 <tanja.landolt@zhdk.ch>
2022-11-11 22:37:55 +0100

    new changes

--- 06_Ex_Homework/sketch.js
+++ 06_Ex_Homework/sketch.js
@@ -8,7 +8,6 @@ let table;
 // for clouds
 let cloudx = 100;
 let cloudy = 100;
-let _scale = 1;
 
 // for font 
 let myFont;
@@ -24,6 +23,7 @@ function preload() {
 
 function setup() {
   createCanvas(1000, 1000);
+  background('#D4B8B1');
 
   const barMargin = 10;
   const barHeight = 30;
@@ -46,15 +46,7 @@ function setup() {
 
     
     // console.log(change);
-    push();
-  textFont(myFont);
-  fill ('#000000');
-    text(city, 
-         20,
-         100 + 100 * i++, 
-         80, 
-         30);
-         pop(); 
+
       
        
 
@@ -64,9 +56,9 @@ function setup() {
                          20, // smallest circle
                          130); // largest circle
 
-    fill ('#2EDFF2'); 
-    noStroke()
-    ellipse(120, 100 + 100 * i, circleSize);
+   // fill ('#2EDFF2'); 
+   // noStroke()
+   // ellipse(120, 100 + 100 * i, circleSize);
     
     
     // var change =  futurewettestMonth / wettestMonth
@@ -82,21 +74,37 @@ function setup() {
     
 
     // console.log(wettestMonth * change)
-    fill ('#A64600');
+    fill ('#D4B8B1');
     noStroke();
-    ellipse(220, 100 + 100 * i, circleSize2);
+  //  ellipse(220, 100 + 100 * i, circleSize2);
     //ellipse(240, 120 + 100 * i, circleSize2 * 1.75);
    // ellipse(230, 90 + 100 * i, circleSize2 * 1.5);
    
 
-
+ 
    // cloud function
-    // makeCloud(120, 100 + 100 * i, circleSize);
-    // fill(159, 115, 171);
-    // makeCloud(220, 100 + 100 * i, circleSize2)
-    // fill(63, 59, 108);
-    // noStroke();
+   fill(205, 140, 140, 150);
+   makeCloud(120, 100 + 100 * i, circleSize);
+   
+    fill(83, 51, 31, 150);
+    makeCloud(220, 100 + 100 * i, circleSize2);
     
+    noStroke();
+
+
+
+
+    push();
+    textFont(myFont);
+    textSize(18);
+    fill ('black');
+      text(city, 
+           135,
+           90 + 100 * i, 
+           80, 
+           30);
+           pop(); 
+   
   }
 
 }
@@ -113,10 +121,11 @@ function draw() {
 }
 
 // function to make clouds out of ellipse
-function makeCloud(cloudx, cloudy, _scale) {
+function makeCloud(cloudx, cloudy, size) {
   //fill(63, 59, 108);
   noStroke();
-  ellipse(cloudx, cloudy, _scale*70, _scale);
-  ellipse(cloudx + 10, cloudy + 10, _scale, _scale);
-  ellipse(cloudx - 20, cloudy + 10, _scale, _scale);
+  ellipse(cloudx, cloudy, size, size);
+  ellipse(cloudx + 10, cloudy + 0.2, size, size);
+  ellipse(cloudx - 20, cloudy + 10, size, size);
+  ellipse(cloudx - 10, cloudy -15, size, size);
 }
